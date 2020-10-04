9ac4c19c12704977bd348f7fbe81f622dad2d3ea
KushagraMimani <69343603+KushagraMimani@users.noreply.github.com>
2020-10-04 18:45:44 +0530

    Add files via upload

---
+++ hexagon.js
@@ -0,0 +1,22 @@
+class Hexagon{
+    constructor(x, y) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.circle(x, y, 40, options);
+        this.radius = 40;
+        this.image = loadImage("image/hexagon.png");
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.radius, this.radius);
+        pop();
+      }
+}
